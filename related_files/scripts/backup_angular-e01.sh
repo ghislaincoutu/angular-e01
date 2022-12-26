18b01fb4d489af48fff81a9592c48a6c4a1252d2
ghislain.coutu <gc8395@gmail.com>
2022-12-26 13:18:17 -0500

    Exercice Angular

--- related_files/scripts/backup_angular-e01.sh
+++ related_files/scripts/backup_angular-e01.sh
@@ -6,10 +6,10 @@ echo "Copie de sécurité des fichiers relatifs à l'application angular-e01"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 source=/var/www/html/r002/angular-e01
-destination=/home/Exercices/angular-e01/related_files/publication
+destination=/home/Exercises/angular-e01/related_files/publication
 rm $destination/*.tar.gz
 tar -czvf $destination/pub_angular-e01_`(date +%Y%m%d-%H%M)`.tar.gz $source/
 
-source=/home/Exercices/angular-e01/
-destination=/home/CopiesSecurite/angular-e01_`(date +%Y%m%d-%H%M)`.tar.gz
+source=/home/Exercises/angular-e01/
+destination=/home/Backups/angular-e01_`(date +%Y%m%d-%H%M)`.tar.gz
 tar --exclude=".angular" --exclude=".git" --exclude="node_modules" -czvf $destination $source
--- related_files/scripts/publish_application.sh
+++ related_files/scripts/publish_application.sh
@@ -6,7 +6,7 @@ echo "Publication de l'applications sur le serveur Web local"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
-so=/home/Exercices/angular-e01/dist/angular-e01
+so=/home/Exercises/angular-e01/dist/angular-e01
 # Destination (de)
 de=/var/www/html/r002/angular-e01
 
